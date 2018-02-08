38c000cf495658dac9fdac753b377f2c159638e4
Paweł Bartkiewicz <tuuresairon@gmail.com>
2018-02-08 23:27:22 +0100

    Refactor SbtrunfastPlugin, add runfastFile and runfastClasspath keys

--- src/main/scala/pl/tues/sbt/runfast/SbtrunfastPlugin.scala
+++ src/main/scala/pl/tues/sbt/runfast/SbtrunfastPlugin.scala
@@ -12,32 +12,41 @@ object SbtrunfastPlugin extends AutoPlugin {
   override def requires = JvmPlugin
 
   object autoImport {
-    val runfastGenerate = taskKey[Option[File]]("Generates a runfast script in the target directory")
+    val runfastGenerate  = taskKey[Option[File]]("Generates a runfast script in the target directory")
+    val runfastFile      = settingKey[File]("File to which the script should be written")
+    val runfastClasspath = taskKey[Seq[File]]("Classpath used by the script")
   }
 
   import autoImport._
 
   override lazy val projectSettings = Seq(
-    runfastGenerate := (mainClass in Compile).value.map { main =>
-      val runfastFile = (target in Compile).value / "runfast.sh"
-      val classpath = (fullClasspath in Runtime).value.map(_.data).mkString(":")
-
-      IO.writeLines(runfastFile,
-        Seq(
-          "#!/bin/bash",
-          "",
-          s"""java -cp '${classpath}' '${main}' "$$@""""
-        )
-      )
-
-      Process(Seq("chmod", "a+x", runfastFile.getPath())).!
-
-      runfastFile
-    }
+    runfastFile      := (target in Compile).value / "runfast.sh",
+    runfastClasspath := (fullClasspath in Runtime).value.map(_.data),
+    runfastGenerate  := (mainClass in Compile).value.map(
+      RunfastGenerate(_, runfastFile.value, runfastClasspath.value)
+    )
   )
 
   override lazy val buildSettings = Seq()
 
   override lazy val globalSettings = Seq()
+
+}
+
+object RunfastGenerate {
+
+  def apply(main: String, runfastFile: File, classpath: Seq[File]): File = {
+    IO.writeLines(runfastFile,
+      Seq(
+        "#!/bin/bash",
+        "",
+        s"""java -cp '${classpath.mkString(":")}' '${main}' "$$@""""
+      )
+    )
+
+    Process(Seq("chmod", "a+x", runfastFile.getPath())).!
+
+    runfastFile
+  }
 
 }
